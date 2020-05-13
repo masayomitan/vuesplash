2ad1bbdef42127e0a39f553045985c4c2b9ad3fe
kawasaki <yomitanmasa3523@gmail.com>
2020-05-13 17:51:10 +0900

    vuex

---
+++ resources/js/store/auth.js
@@ -0,0 +1,18 @@
+const state = {}
+
+const getters = {}
+
+const mutations = {}
+
+const actions = {}
+
+
+expert default {
+
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+  
+}
