17ec229dc194aee41e1ef58f1ed6bf041f0370b0
kawasaki <yomitanmasa3523@gmail.com>
2020-05-15 09:50:59 +0900

    セキュリティ

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -14,17 +14,21 @@ const actions = {
   async register (context, data) {
     const response = await axios.post('/api/register', data)
     context.commit('setUser', response.data)
+  },
+  async login (context, data) {
+    const response = await axios.post('/api/login', data)
+    context.commit('setUser', response.data)
+  },
+  async logout (context) {
+    const response = await axios.post('/api/logout')
+    context.commit('setUser', null)
   }
 }
 
-
-
-expert default {
-
+export default {
   namespaced: true,
   state,
   getters,
   mutations,
   actions
-
 }
