b1ca55a9b647a4add97ea31a52cac2cc16c0b31f
kawasaki <yomitanmasa3523@gmail.com>
2020-05-16 13:09:54 +0900

    ayth.js

--- resources/js/router.js
+++ resources/js/router.js
@@ -6,6 +6,8 @@ import VueRouter from 'vue-router'
 import PhotoList from './pages/PhotoList.vue'
 import Login from './pages/Login.vue'
 
+import store from './store'
+
 
 
 // VueRouterプラグインを使用する
@@ -20,7 +22,14 @@ const routes = [
   },
   {
     path: '/login',
-    component: Login
+    component: Login,
+    beforeEnter (to, from, next) {
+      if (store.getters['auth/check']) {
+        next('/')
+      } else {
+        next()
+      }
+    }
   }
 ]
 
